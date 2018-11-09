063d14f1cea590b5d0a03de311cfa97005d5ada9
braitsch <stephen@braitsch.io>
2018-11-09 12:00:38 -0800

    cropping finally working correctly, accounting for resizable container

--- public/js/p1.js
+++ public/js/p1.js
@@ -1,20 +1,31 @@
 
 $(function() {
 
-	const maxFileSize = 1;
+	let thumb = {};
+	let image = {};
 	let file = { url : '/img/slow-magic.jpg' };
 
-	let image = $('img');
-	let btnSelect = $('.btn-select');
-	let btnUpload = $('.btn-upload');
-	let fileDialog = $('.file-dialog');
+	const maxFileSize = 1;
+
+	const $viewer = $('.viewer');
+	const $image = $('.viewer img');
+	const $marquee = $('.marquee');
+	const btnSelect = $('.btn-select');
+	const btnUpload = $('.btn-upload');
+	const fileDialog = $('.file-dialog');
 
 	btnSelect.click(function(e){  fileDialog.click(); });
 	btnUpload.click(function(e){
+		let mx = (image.nw/image.w);
+		let my = (image.nh/image.h);
+		thumb.crop.x = (thumb.crop.x-image.x) * mx;
+		thumb.crop.y = (thumb.crop.y-image.y) * my;
+		thumb.crop.w *= mx;
+		thumb.crop.h *= my;
 		var request = new XMLHttpRequest();
 			request.open('POST', '/upload');
 		var formData = new FormData();
-			formData.append('crop', JSON.stringify(thumb));
+			formData.append('data', JSON.stringify(thumb.crop));
 			formData.append('file', new File([file.blob], 'photo.'+file.ext, { type: 'image/'+(file.ext=='jpg' ? 'jpeg' : file.ext)}));
 		request.send(formData);
 	})
@@ -42,11 +53,32 @@ $(function() {
 		file.blob = blob;
 		var reader = new FileReader();
 		reader.onload = function (e) {
-			image.attr('src', e.target.result);
+			$image.attr('src', e.target.result);
 			setTimeout(function(){
-				let img = image[0];
-			// calculate computed position from object-fit //
-				console.log(img.width, img.height, img.naturalWidth, img.naturalHeight)
+				let img = $image[0];
+				let dw = img.width/img.naturalWidth;
+				let dh = img.height/img.naturalHeight;
+				if (dw > dh){
+					let w = img.naturalWidth * dh;
+					image = {
+						x	:(img.width-w)/2,
+						y	:0,
+						w	:w,
+						h	:img.height
+					};
+				}	else{
+					let h = img.naturalHeight * dw;
+					image = {
+						x	:0,
+						y	:(img.height-h)/2,
+						w	:img.width,
+						h	:h
+					};
+				}
+				image.nw = img.naturalWidth;
+				image.nh = img.naturalHeight;
+				//$marquee.css({top:image.y, left:image.x, width:image.w, height:image.h});
+				//$marquee.show();
 			}, 100)
 		}
 		reader.readAsDataURL(blob);
@@ -57,87 +89,85 @@ $(function() {
 	/*
 		thumbnail generator
 	*/
-	
-	let thumb = {};
-	var $image = $('.viewer img');
-	var $thumb = $('.marquee');
-	var $container = $('.viewer');
 
-	$thumb.hide();
+
+	$marquee.hide();
 	$image.on('dragstart', function(e) { e.preventDefault(); });
 	var clearThumbnail = function()
 	{
-		$thumb.hide();
+		$marquee.hide();
 		thumb.crop = { };
 	}
 	var getMousePosition = function(e)
 	{
 		var mouse = { x:e.clientX, y:e.clientY };
-		var div = {x:$container.offset().left - $(window).scrollLeft(), y:$container.offset().top - $(window).scrollTop()};
+		var div = {x:$viewer.offset().left - $(window).scrollLeft(), y:$viewer.offset().top - $(window).scrollTop()};
 		return { x:(mouse.x - div.x), y:(mouse.y - div.y) };
 	}
 	var resize = function(e)
 	{
 		var mouse = getMousePosition(e);
-		var width = mouse.x - thumb.crop.x;
-		var height = mouse.y - thumb.crop.y;
+		if (!isWithinBounds(mouse)) return;
+		thumb.crop.w = mouse.x - thumb.crop.x;
+		thumb.crop.h = mouse.y - thumb.crop.y;
 		if (thumb.mode == 'fixed ratio'){
-			height = width * (thumb.ratio.height/thumb.ratio.width);
+			thumb.crop.h = thumb.crop.w * (thumb.ratio.height/thumb.ratio.width);
 		}
-		$thumb.css({
-			width	:width,
-			height	:height
-		});
+		$marquee.css({ width:thumb.crop.w, height:thumb.crop.h});
+	}
+	var isWithinBounds = function(p)
+	{
+		let x = p.x-image.x > 0;
+		let y = p.y-image.y > 0;
+		let w = p.x<(image.x+image.w);
+		let h = p.y<(image.y+image.h);
+		return x && y && w && h;
 	}
 	var onMouseDown = function(e)
 	{
 		var mouse = getMousePosition(e);
-		thumb.crop = { };
-		thumb.crop.x = mouse.x;
-		thumb.crop.y = mouse.y;
-		$thumb.css({
-			left	:thumb.crop.x,
-			top		:thumb.crop.y,
-		});
+		if (!isWithinBounds(mouse)) return;
+		thumb.crop = { x:mouse.x, y:mouse.y };
+		$marquee.css({ left:thumb.crop.x, top:thumb.crop.y});
 	// allow resizing //
 		if (thumb.mode != 'fixed size'){
 			$image.bind("mousemove", resize);
-			$thumb.bind("mousemove", resize);
+			$marquee.bind("mousemove", resize);
 			$image.css('cursor', 'crosshair');
-			$thumb.css({ width:0, height:0 });
+			$marquee.css({ width: 0, height: 0 });
 		}	else{
 	// fixed size mode //
-			$thumb.css({
+			$marquee.css({
 				width	:thumb.width * ($image.width()/image.width),
 				height	:thumb.height * ($image.height()/image.height)
 			});
 		}
-		$thumb.show();
+		$marquee.show();
 	}
 	var onMouseUp = function()
 	{
 		$image.css('cursor', 'default');
 		$image.unbind("mousemove", resize);
-		$thumb.unbind("mousemove", resize);
+		$marquee.unbind("mousemove", resize);
 	}
 
 	$(window).bind("mouseup", onMouseUp);
 	$image.bind("mousedown", onMouseDown);
-	$thumb.bind("mousedown", onMouseDown);
-	document.addEventListener("keydown", function(e){
-// allow arrow keys to fine tune placement of the thumbnail //
-		if ($thumb.is(":visible")){
-			if (e.keyCode == 37){
-				thumb.crop.x--;
-			} else if (e.keyCode == 38){
-				thumb.crop.y--;
-			} else if (e.keyCode == 39){
-				thumb.crop.x++;
-			} else if (e.keyCode == 40){
-				thumb.crop.y++;
-			}
-			$thumb.css({ left:thumb.crop.x, top:thumb.crop.y });
-		}
-	}, false);
+	$marquee.bind("mousedown", onMouseDown);
+// 	document.addEventListener("keydown", function(e){
+// // allow arrow keys to fine tune placement of the thumbnail //
+// 		if ($marquee.is(":visible")){
+// 			if (e.keyCode == 37){
+// 				thumb.crop.x--;
+// 			} else if (e.keyCode == 38){
+// 				thumb.crop.y--;
+// 			} else if (e.keyCode == 39){
+// 				thumb.crop.x++;
+// 			} else if (e.keyCode == 40){
+// 				thumb.crop.y++;
+// 			}
+// 			$marquee.css({ left:thumb.crop.x, top:thumb.crop.y });
+// 		}
+// 	}, false);
 
 });
--- server/routes.js
+++ server/routes.js
@@ -1,80 +1,100 @@
 
 const fs = require('fs');
 const path = require('path');
+const sharp = require('sharp');
 const formidable = require('formidable');
-var pushpop = require('../../pushpop/npm-module/index.js');
+//var pushpop = require('../../pushpop/npm-module/index.js');
 //var pushpop = require('pushpop');
 
-pushpop.config({
-// [required] set the global upload directory //
-	uploads:path.join(__dirname, '..', '/uploads'),
-// [optional] overwrite file names with unique ids //
-	uniqueIds:true,
-// [optional] enable logging //
-	enableLogs:true,
-// [optional] save files to gcloud instead of the local filesystem //
-//	service: { name:'gcloud', bucket:'pushpop'}
-})
+// pushpop.config({
+// // [required] set the global upload directory //
+// 	uploads:path.join(__dirname, '..', '/uploads'),
+// // [optional] overwrite file names with unique ids //
+// 	uniqueIds:true,
+// // [optional] enable logging //
+// 	enableLogs:true,
+// // [optional] save files to gcloud instead of the local filesystem //
+// //	service: { name:'gcloud', bucket:'pushpop'}
+// })
 
 // delete from demo anything that is older than three minutes //
-var CronJob = require('cron').CronJob;
-new CronJob('*/1 * * * *', pushpop.purge, 3, true, 'America/Los_Angeles');
+//var CronJob = require('cron').CronJob;
+//new CronJob('*/1 * * * *', pushpop.purge, 3, true, 'America/Los_Angeles');
 
 module.exports = function(app) {
 
 	app.get('/', function (req, res)
 	{	
-		res.redirect('/project/gallery');
+		res.render('gallery');
+		//res.redirect('/project/gallery');
 	});
 
 	app.get('/project/:id', function(req, res)
 	{
 	// set the active project //
-		pushpop.setProject(req.params['id'], function(project){
-			res.render('gallery', { project : project });
-		});
+		// pushpop.setProject(req.params['id'], function(project){
+		// 	res.render('gallery', { project : project });
+		// });
 	});	
 
 	app.get('/project/:id/print', function(req, res){
-		pushpop.getProject(req.params['id'], function(project){
-			res.send({ project : project });
-		})
+		// pushpop.getProject(req.params['id'], function(project){
+		// 	res.send({ project : project });
+		// })
 	});
 
 	app.get('/print', function(req, res){
-		pushpop.getAll(function(projects){
-			res.send({ projects : projects });
-		})
+		// pushpop.getAll(function(projects){
+		// 	res.send({ projects : projects });
+		// })
 	});
 
 	app.get('/reset', function(req, res){
-		pushpop.reset(function(){
-			res.redirect('/');
-		});
+		// pushpop.reset(function(){
+		// 	res.redirect('/');
+		// });
 	});
 
-	app.post('/delete', pushpop.delete, function(req, res)
+	app.post('/delete', function(req, res)
 	{
-		if (!pushpop.error){
-			res.send('ok').status(200);
-		}	else{
-			res.send(pushpop.error).status(500);
-		}
+		// if (!pushpop.error){
+		// 	res.send('ok').status(200);
+		// }	else{
+		// 	res.send(pushpop.error).status(500);
+		// }
 	});
 
 	app.post('/upload', function(req, res)
 	{
-		var form = new formidable.IncomingForm();
-		var fileName = app.guid()
+		let form = new formidable.IncomingForm();
+		let fileName = app.guid();
+		let cropData = undefined;
+		const gallery =  __dirname + '/../uploads';
 		form.on('file', function(type, file) {
-			const gallery =  __dirname + '/../uploads';
 		// rename the incoming webm stream file so it is unique before we save it to disk //
 			fs.rename(file.path, gallery +'/'+ fileName + '.jpg', function( e ) { });
 		});
 		form.on('field', function(name, field) {
-			console.log('field', name, JSON.parse(field))
+			if (name == 'data') cropData = JSON.parse(field);
 		});
-		form.on('end', function() { res.send('ok');});
+		form.on('end', function() {
+			if (cropData){
+				let large = gallery +'/'+ fileName + '.jpg'
+				let small = gallery +'/'+ fileName + '_sm.jpg'
+				sharp(large)
+				.extract({
+					left: Math.round(cropData.x),
+					top: Math.round(cropData.y),
+					width: Math.round(cropData.w),
+					height: Math.round(cropData.h)
+				}).toBuffer().then(data => {
+					sharp(data).toFile(small).then(function(){
+						console.log('thumbnail generated')
+					});
+				});
+			}
+			res.send('ok');
+		});
 		form.parse(req, function(err, fields, file) { });
 	});
 
