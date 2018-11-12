b1e54d77f4b5119c9cb6af9ac23f617305977bff
braitsch <stephen@braitsch.io>
2018-11-11 16:24:46 -0800

    cleaning up & calling it v1

--- app.js
+++ app.js
@@ -2,22 +2,16 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-// bodyParser only required for stripe integration //
-var bodyParser = require('body-parser'); 
 
 app.locals.pretty = true;
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'pug');
 app.set('views', './server/views');
-// bodyParser only required for stripe integration //
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/uploads'));
 
 require('./server/routes')(app);
-require('./server/stripe')(app);
 
 const exec = require('child_process').exec;
 const cmpl = 'stylus -w '+__dirname+'/public/css';
