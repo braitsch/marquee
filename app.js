07c278e938fc9f473822f690eeebb980479793f5
braitsch <stephen@braitsch.io>
2018-11-12 15:59:00 -0800

    removing stylus compiler from app.js

--- app.js
+++ app.js
@@ -4,20 +4,12 @@ var app = express();
 var http = require('http').Server(app);
 
 app.locals.pretty = true;
-
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'pug');
 app.set('views', './server/views');
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/uploads'));
 
 require('./server/routes')(app);
-
-const exec = require('child_process').exec;
-const cmpl = 'stylus -w '+__dirname+'/public/css';
-exec( cmpl, (e, stdout, stderr) => {
-	e ? console.log(e) : console.log('watching style sheet');
-});
 
 http.listen(app.get('port'), function(q, r)
 {
